3f815f94baf8e33b8196188797b1645c563ae98c
matsabino <43677961+matsabino@users.noreply.github.com>
2022-12-18 23:38:47 -0300

    Changing port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const port = 3000;
+const port = process.env.PORT || 3000;
 const ejs = require('ejs');
 var session = require('express-session')
 var bodyParser = require('body-parser')
